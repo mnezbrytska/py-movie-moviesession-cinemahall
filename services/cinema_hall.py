186066a4d1b0eb35f1abca6ca005c047b700bac9
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-08-29 14:47:00 +0300

    "fixed"

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -12,7 +12,7 @@ def create_cinema_hall(
         hall_rows: int,
         hall_seats_in_row: int
 ) -> CinemaHall:
-    cinema_hall = CinemaHall.objects.create(
+    CinemaHall.objects.create(
         name=hall_name,
         rows=hall_rows,
         seats_in_row=hall_seats_in_row
